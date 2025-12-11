9661496debea8d52daf23e80450dab7cb665105c
vouandzanoeldy@gmail.com <vouandzanoeldy@gmail.com>
2025-12-11 15:11:33 +0100

    modif

--- demo/src/test/java/com/example/productTest.java
+++ demo/src/test/java/com/example/productTest.java
@@ -2,13 +2,17 @@ package com.example;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import com.example.pom.loginPf;
 import com.example.pom.productpom;
@@ -17,12 +21,24 @@ public class productTest {
 @Test
 @Tag("@regression")
 public void prod(){
-    WebDriver driver= new FirefoxDriver();
+     URL grid;
+    WebDriver driver= null;
+    try {
+       
+        grid = new URL("http://localhost:4444");
+    ChromeOptions co=new ChromeOptions();
+     driver = new RemoteWebDriver(grid,co);
+    } catch (MalformedURLException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }
     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
    
     loginPf lp=new loginPf(driver);
+   
     productpom pp=new productpom(driver);
-   driver.get("https://www.saucedemo.com/");
+    String url=System.getProperty("url","https://www.saucedemo.com") ;// récuperation de la variable d'environnement
+    driver.get(url);
    lp.saisirusername("standard_user");
    lp.saisirpassword("secret_sauce");
    lp.se_connecter();
